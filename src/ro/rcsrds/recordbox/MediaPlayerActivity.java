907de9741a54b6ea59c7bd43a15ad2542b28849a
Omusoru <Tybyro@gmail.com>
2014-07-11 19:12:47 +0300

    Am adaugat functionalitate la seekbar. -Tibi

--- src/ro/rcsrds/recordbox/MediaPlayerActivity.java
+++ src/ro/rcsrds/recordbox/MediaPlayerActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.util.TimeFormatException;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
