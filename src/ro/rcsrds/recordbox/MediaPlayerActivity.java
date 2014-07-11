3ec9a06cf43ac442f8c00894361dfc344378ec2a
razzzvan321 <razzzvan321@gmail.com>
2014-07-11 17:03:37 +0300

    Removed unused imports

--- src/ro/rcsrds/recordbox/MediaPlayerActivity.java
+++ src/ro/rcsrds/recordbox/MediaPlayerActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MediaPlayerActivity extends Activity {
 	
