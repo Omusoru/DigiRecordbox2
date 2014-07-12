adf32bb2efd9d36c5fc69d176ac9ab8c5bbdafbe
Omusoru <Tybyro@gmail.com>
2014-07-12 18:18:22 +0300

    Com

--- src/ro/rcsrds/recordbox/MediaPlayerActivity.java
+++ src/ro/rcsrds/recordbox/MediaPlayerActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
