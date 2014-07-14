7e56ada02e3428bdcf7f036841d75f41efafd7c0
razzzvan321 <razzzvan321@gmail.com>
2014-07-14 18:51:00 +0300

    Context menu for recording list

--- src/ro/rcsrds/recordbox/RecordingListActivity.java
+++ src/ro/rcsrds/recordbox/RecordingListActivity.java
@@ -6,9 +6,17 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ListView;
 
 public class RecordingListActivity extends Activity {	
@@ -31,6 +39,8 @@ public class RecordingListActivity extends Activity {
 		adapter = new RecordingListAdapter(this, recordingList);
 		list.setAdapter(adapter);
 		list.setOnItemClickListener(new ListOnClickListener());
+		//list.setOnItemLongClickListener(new ListOnLongClickListener());
+		registerForContextMenu(list);
 		
     	    
 }
@@ -41,6 +51,35 @@ public class RecordingListActivity extends Activity {
 		recordingList = new ArrayList<Recording>();
 		recordingList = db.getAllRecordings();
 		
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		if (v.getId()==R.id.list) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+		    menu.setHeaderTitle(recordingList.get(info.position).getName());
+		    String[] menuItems = getResources().getStringArray(R.array.context_recordinglist);
+		    for (int i = 0; i<menuItems.length; i++) {
+		      menu.add(Menu.NONE, i, i, menuItems[i]);
+		    }
+		  
+		}
+	}
+	
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+		int menuItemIndex = item.getItemId();
+		String[] menuItems = getResources().getStringArray(R.array.context_recordinglist);
+		String menuItemName = menuItems[menuItemIndex];
+		String listItemName = recordingList.get(info.position).getName();
+	
+		//text.setText(String.format("Selected %s for item %s", menuItemName, listItemName));
+		Log.d("Recordinglist","Selected "+menuItemName+" for item "+listItemName);
+		
+		return super.onContextItemSelected(item);
 	}
 	
 	private class ListOnClickListener implements OnItemClickListener {
@@ -54,9 +93,23 @@ public class RecordingListActivity extends Activity {
 			startActivity(intent);
 		}
 		
-		
-		
+			
 	}
+	
+//	private class ListOnLongClickListener implements OnItemLongClickListener {
+//
+//		@Override
+//		public boolean onItemLongClick(AdapterView<?> parent, View view,
+//				int position, long id) {
+//			//MenuInflater inflater = new MenuInflater(this);
+//			
+//			return false;
+//		}
+//
+//		
+//		
+//	
+//	}
 	
 	
 
