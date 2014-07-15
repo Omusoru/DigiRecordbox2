0cc16ba8a6be2f61fcf934d34c3c8816db81b8ab
Omusoru <Tybyro@gmail.com>
2014-07-15 16:03:31 +0300

    Modified upload for api

--- src/ro/rcsrds/recordbox/FileManager.java
+++ src/ro/rcsrds/recordbox/FileManager.java
@@ -1,7 +1,14 @@
 package ro.rcsrds.recordbox;
 
 import java.io.IOException;
+import java.util.List;
 
+import net.koofr.api.v2.DefaultClientFactory;
+import net.koofr.api.v2.StorageApi;
+import net.koofr.api.v2.StorageApiException;
+import net.koofr.api.v2.resources.Mount;
+import net.koofr.api.v2.transfer.upload.FileUploadData;
+import net.koofr.api.v2.transfer.upload.UploadData;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Environment;
@@ -11,34 +18,49 @@ public class FileManager {
 	public static final String PREFS_NAME = "Authentication";
 	private SharedPreferences preferences;
 	private String localFilePath;
+	private String username;
+	private String password;
+	private Mount mount;
+	private StorageApi api;
 	
 	public FileManager(Context context){				
 		preferences = context.getSharedPreferences(PREFS_NAME, 0);
-		String username = preferences.getString("username", "").toLowerCase();
+		username = preferences.getString("username", "").toLowerCase();
 		username = username.replace(".", "DOT");
 		username = username.replace("@", "AT");
 		localFilePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/DigiRecordbox/"+username+"/";
+		username = preferences.getString("username", "");
+		password = preferences.getString("password", "");
+		try {
+			api = DefaultClientFactory.create("storage.rcs-rds.ro",username, password);
+		} catch (StorageApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			mount = api.getMounts().get(0);
+		} catch (StorageApiException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean upload(String file){
 
-		file = localFilePath + file;		
-		DigiFTPClient ftp=new DigiFTPClient("storage.rcs-rds.ro",21,preferences.getString("username", ""),preferences.getString("password", ""));
+		file = localFilePath + file;
+		file.replace("/", "\\");
+		
 		try {
-			ftp.connect();
-			ftp.changeWorkingDir("Digi Cloud");
-			if(!ftp.dirExists("DigiRecordbox")){
-				ftp.makeDir("DigiRecordbox");
-			}
-			ftp.changeWorkingDir("DigiRecordbox");
-			ftp.putFile(file,".");
-			ftp.disconnect();
+			api.createFolder(mount.getId(), "/", "DigiRecordbox");
+			UploadData data = new FileUploadData(file);  
+			api.filesUpload(mount.getId(), "/DigiRecordbox", data, new SimpleProgressListener());
 			return true;
-		} catch (IOException e) {
+		} catch (StorageApiException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}
+
 	}
 	
 	public boolean download(String file){
